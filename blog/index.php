ae2dc0e22ab8871193ee162cd772ecf6fce7dd57
Benokan <benokankafkas@gmail.com>
2018-02-06 00:28:14 +0300

    1
    First and probably last

---
+++ blog/index.php
@@ -0,0 +1,67 @@
+<?php
+include_once('resources/init.php');
+//$posts = (isset($_GET['id'])) ? get_posts($_GET['id']) : get_posts();
+$posts = get_posts((isset($_GET['id']))? $_GET['id'] : null); 
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Blog</title>
+        <script>
+		setInterval(function(){
+    	var x = Math.round( Math.random() * 255 );
+    	var y = Math.round( Math.random() * 255 );
+    	var z = Math.round( Math.random() * 255 );
+    	var bg = "background:rgb("+x+", "+y+", "+z+");";
+    	var element = document.getElementById("random-background");
+   	    element.style = bg;
+		
+		}, 1000);
+		</script>
+
+
+
+        <style>
+        	#random-background {
+   			width: 100vw;
+    		height: 100vh;
+   		    background: white;
+   		    margin: 0px;
+   		    transition: 1s;
+}
+            ul{list-style: none;}
+            li{display: inline; margin-right: 20px;}
+        </style>
+    </head>
+    <body id = "random-background">
+     <nav>
+        <ul>
+            <li><a href='index.php' >Index</a></li>
+            <li><a href='add_post.php' >Add a Post</a></li>
+            <li><a href='add_category.php' >Add Category</a></li>
+            <li><a href='category_list.php' >Category List</a></li>
+            <!--li><a href='' ></a></li-->
+        </ul>
+     </nav>
+     <h1>Welcome to Benokan's BLOG</h1>
+     
+     <?php
+     foreach($posts as $post){
+      ?>
+     <h2><a href='index.php?id=<?php echo $post['post_id']; ?>' ><?php echo $post['title']; ?></a></h2>
+     <p>
+        Posted on <?php echo date('d-m-y h:i:s',strtotime($post['date_posted'])); ?>
+        In <a href='category.php?id=<?php echo $post['category_id']; ?>' ><?php echo $post['name']; ?></a>
+     </p>
+     <div><?php echo nl2br($post['contents']); ?></div>
+     <menu>
+        <ul>
+            <li><a href='delete_post.php?id=<?php echo $post['post_id']; ?>' >Delete This Post</a></li>
+            <li><a href='edit_post.php?id=<?php echo $post['post_id']; ?>' >Edit This Post</a></li>
+        </ul>
+     </menu>
+     <?php   
+     }
+     ?>
+    </body>
+</html>
